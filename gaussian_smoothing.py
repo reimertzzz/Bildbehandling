5358b9333d4d92735ae0a6fecd70e239e5b2326d
Mattias Michaelsson <matmic@chalmers.se>
2024-10-03 11:16:43 +0200

    Add sigma parameter gaussian smoothing

--- gaussian_smoothing.py
+++ gaussian_smoothing.py
@@ -4,15 +4,14 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-def gaussian_smooth(img_path):
+def gaussian_smooth(img_path, sigma):
     img_gray = Image.open(img_path).convert('L')
     np_image = np.asarray(img_gray)
-    std_deviation = np.std(img_gray)
-    smoothed_img = gaussian_filter(np_image, std_deviation)
+    smoothed_img = gaussian_filter(np_image, sigma)
     return smoothed_img
 
-def display_smooth_img(img_path):
-    img = gaussian_smooth(img_path)
+def display_smooth_img(img_path, sigma=8.5):
+    img = gaussian_smooth(img_path, sigma)
     plt.imshow(img, cmap='grey')
     plt.axis('on')
     plt.title('Gaussian smoothed image')
